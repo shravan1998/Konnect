3b88bda4e6e422e5440e404dcf7134349540adc6
shravan1998 <shrkum51@gmail.com>
2020-08-28 23:35:58 +0530

    Added the first register page

---
+++ konnect/src/views/Register.vue
@@ -0,0 +1,38 @@
+<template>
+
+<v-container>
+
+<div class="card">
+  <div class="card-body">
+<form>
+  <h3>Register</h3>
+  <div class="row">
+    <div class="col">
+      <input type="text" class="form-control" placeholder="First name">
+    </div>
+    <div class="col">
+      <input type="text" class="form-control" placeholder="Last name">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" >
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+    <div class="form-group">
+    <label for="exampleInputPassword1">Confirm Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+  &nbsp;
+  <router-link class="btn btn-success" tag="button" to="/register">Register</router-link>
+   
+</form>
+  </div>
+</div>
+</v-container>
+</template>
