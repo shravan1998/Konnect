e511e704f8955f554606b500466b5349926b5137
shravan1998 <shrkum51@gmail.com>
2020-08-30 16:13:36 +0530

    Added profile page

---
+++ konnect/src/views/Profile.vue
@@ -0,0 +1,122 @@
+<template>
+   
+    <div id="app">
+       
+    
+        
+  <v-app id="inspire"
+  style="margin-top:-25px;"
+  >
+        <Navbar />
+        <v-container
+        style="position:absolute;top:10%;"
+        >
+         <v-card
+    class="mx-auto"
+    max-width="800"
+    
+    tile
+  >
+    <v-img
+      height="100%"
+      src="https://cdn.vuetifyjs.com/images/cards/server-room.jpg"
+    >
+      <v-row
+        align="end"
+        class="fill-height"
+      >
+        <v-col
+          align-self="start"
+          class="pa-0"
+          cols="12"
+        >
+          <v-avatar
+            class="profile"
+            color="grey"
+            size="164"
+            tile
+          >
+            <v-img src="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg"></v-img>
+          </v-avatar>
+        </v-col>
+        <v-col class="py-0">
+          <v-list-item
+            color="rgba(0, 0, 0, .4)"
+            dark
+          >
+            <v-list-item-content>
+              <v-list-item-title class="title">Marcus Obrien</v-list-item-title>
+              <v-list-item-subtitle>Network Engineer</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-col>
+        <v-col>
+            <button type="button" class="btn clr">Add Friend</button>
+            &nbsp;
+            <button type="button" class="btn clr">Message</button>
+        </v-col>
+      </v-row>
+    </v-img>
+  </v-card>
+           <form>
+          <div class="form-group">
+             <label for="exampleFormControlTextarea1">Post something here</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+      <v-card
+    class="mx-auto"
+    max-width="344"
+  >
+    <v-card-text>
+      <div>Word of the Day</div>
+      <p class="display-1 text--primary">
+        be•nev•o•lent
+      </p>
+      <p>adjective</p>
+      <div class="text--primary">
+        well meaning and kindly.<br>
+        "a benevolent smile"
+      </div>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn
+        text
+        color="deep-purple accent-4"
+      >
+        Learn More
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+        </v-container>
+       
+  </v-app>
+ 
+    </div>
+
+</template>
+<script>
+import Navbar from '../components/Navbar'
+//import axios from 'axios'
+export default {
+    name:'Profile',
+    components:{
+        Navbar
+    },
+    data(){
+      return{
+       // slackId:this.$localStorage.get('slackId'),
+        tasks:null
+      }
+    },
+
+    
+}
+</script>
+
+<style scoped>
+.clr{
+    background-color: #ffd11a;
+}
+</style>
