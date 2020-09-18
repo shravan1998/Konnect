c326b908a4838244da28f95c5c0885e6d60b2422
shravan1998 <shrkum51@gmail.com>
2020-09-18 23:07:47 +0530

    Added backend code for login

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,59 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-const mysql = require("mysql");
+const mongo = require("mongoose");
 var cors = require("cors");
 const md5 = require("md5");
 
 app.use(cors());
 app.use(bodyParser());
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit:'5mb'}));
 app.use(bodyParser.urlencoded({extended:false}));
 
 
+var db = mongo.connect("localhost:27017/konnect",function(err){
+    if(err){
+        console.log(err);
+    }else{
+        console.log("Connected to db");
+    }
+});
+var app = express();
+var router = express.Router();
 
+app.use(function(req,res,next){
+    res.setHeader('Access-Control-Allow-Origin','http://localhost:4200');
+    res.setHeader('Allow-Access-Allow-Methods','GET,POST,OPTIONS,PUT,PATCH,DELETE');
+    res.setHeader('Allow-Access-Allow-Headers','Origin,X-Requested-With,Content-Type,Accept');
+    res.setHeader("Access-Control-Allow-Credentials",true);
+    next();
+});
 
+var Schema = mongo.Schema;
 
+var users = new Schema({
+    id:{type:Number},
+    fname:{type:String},
+    lname:{type:String},
+    email:{type:String},
+    password:{type:String}
+});
 
+var model = mongo.model('users',users);
 
-
+app.post('/login',function(req,res){
+    console.log(req.body);
+    var mod = new model(req.body);
+    mod.save(function(err,data){
+        if(err){
+            res.send(err);
+        }
+        else{
+            res.send({data:"Record Inserted"});
+            console.log(data);
+        }
+    });
+});
 
 app.listen(8000,()=>{
     console.log("Backend running at port 8000");
