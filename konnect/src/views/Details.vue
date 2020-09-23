97a1406ab09423c62c8204caad0e53ec57e73d64
shravan1998 <shrkum51@gmail.com>
2020-09-24 00:30:44 +0530

    Created form for details

--- konnect/src/views/Details.vue
+++ konnect/src/views/Details.vue
@@ -8,7 +8,7 @@
   style="margin-top:-25px;"
   >
         <Navbar />
-        <form style="position:absolute;top:10%;right:45%;">
+        <form style="position:absolute;top:10%;">
             <div class="form-row">
                 <div class="col">
                 <label for="dob">Date Of Birth</label>
@@ -44,16 +44,97 @@
                 Others
             </label>
             </div>
-            <v-btn class="mx-2" fab dark color="blue" @click="addEducation">
+            <v-btn class="mx-2" fab dark color="blue" @click="addEducation()">
             <v-icon dark>mdi-plus</v-icon>
             
             </v-btn>Education Details
-        <div v-for="(ed,index) in Education" :key="index">
-            
-            <input type="text" :v-model="ed.value">
-             <button @click="deleteFind(index)">
+            <br>
+        <div class="form-row" v-for="(ed,index) in Education" :key="index">
+            <label for="institute">Institute</label>
+            <input class="form-control" id="institute" type="text" :v-model="ed.institute+index" >
+            <br>
+            <div class="col">
+                <label for="dob">From</label>
+                <input type="date" class="form-control" id="from">
+            </div>
+            <div class="col">
+                <label for="dob">To</label>
+                <input type="date" class="form-control" id="to">
+            </div>
+            <br>
+            <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+            <label class="form-check-label" for="defaultCheck1">
+                I currently study here
+            </label>
+            </div>
+            <br>
+             <button @click="deleteFind()" class="btn btn-primary">
+                 -
             </button>
         </div>
+        <br>
+         <v-btn class="mx-2" fab dark color="blue" @click="addWork()">
+            <v-icon dark>mdi-plus</v-icon>
+            
+            </v-btn>Work Details
+            <br>
+            <div class="form-row" v-for="(ed,index) in Work" :key="index">
+            <label for="institute">Company</label>
+            <input class="form-control" id="institute" type="text" :v-model="ed.company+index" >
+            <br>
+            <div class="col">
+                <label for="dob">From</label>
+                <input type="date" class="form-control" id="from">
+            </div>
+            <div class="col">
+                <label for="dob">To</label>
+                <input type="date" class="form-control" id="to">
+            </div>
+            <br>
+            <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+            <label class="form-check-label" for="defaultCheck1">
+                I currently work here
+            </label>
+            </div>
+            <br>
+             <button @click="deleteWork()" class="btn btn-primary">
+                 -
+            </button>
+        </div>
+        <div class="form-group">
+            <label for="music">Profile Music</label>
+            <input class="form-control" type="text" placeholder="Enter Spotify URL">
+        </div>
+
+  <v-combobox
+    v-model="chips"
+    :items="items"
+    chips
+    clearable
+    label="Enter interests"
+    multiple
+   
+    solo
+  >
+    <template v-slot:selection="{ attrs, item, select, selected }">
+      <v-chip
+        v-bind="attrs"
+        :input-value="selected"
+        close
+        @click="select"
+        @click:close="remove(item)"
+      >
+        <strong>{{ item }}</strong>&nbsp;
+        <span>(interest)</span>
+      </v-chip>
+    </template>
+  </v-combobox>
+
+    <button type="submit" class="btn btn-primary">
+               Submit
+            </button>
            
         </form>
        
@@ -73,13 +154,31 @@ export default {
     data(){
       return{
        // slackId:this.$localStorage.get('slackId'),
-        Education:[]
+        Education:[],
+        Work:[],
+        chips: ['Programming', 'Playing video games', 'Watching movies', 'Sleeping'],
+        items: ['Streaming', 'Eating'],
       }
     },methods:{
         addEducation(){
             
-            this.Education.push({value:''});
-        }
+            this.Education.push({institute:'institute',to:'to',for:'for',current:'current'});
+            //console.log(this.Education);
+        },
+        deleteFind(){
+            this.Education.pop();
+        },addWork(){
+            
+            this.Work.push({company:'company',to:'to',for:'for',current:'current'});
+            console.log(this.Work);
+        },
+        deleteWork(){
+            this.Work.pop();
+        },
+        remove (item) {
+        this.chips.splice(this.chips.indexOf(item), 1)
+        this.chips = [...this.chips]
+      },
     }
 
     
